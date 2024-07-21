fb57f753bfdf3b9fa3c6f20aeb22dea1b4afa067
ionrusu11 <ion.rusu-discultu@student.tuiasi.ro>
2024-07-21 22:36:31 +0300

    Add NuxtUiKit to project

--- front/pages/home.vue
+++ front/pages/home.vue
@@ -1,21 +1,17 @@
 <template>
 	<div class="pl-2">
 		<h2>home page</h2>
-		<pre>{{ data }}</pre>
+		<pre>{{ posts }}</pre>
 	</div>
 
-	<div v-if="status === 'pending'">
-		Loading ...
-	</div>
+	<div v-if="status === 'pending'">Loading ...</div>
 	<div v-else>
 		<div v-for="item in posts">
-			<h1>title: {{item.title}}</h1>
+			<h1>title: {{ item.title }}</h1>
 		</div>
 	</div>
 </template>
 
-
 <script setup lang="ts">
-
-const { status, data: posts } = await useFetch('/api/posts', {lazy: true})
+const { status, data: posts } = await useFetch('/api/posts', { lazy: true })
 </script>
