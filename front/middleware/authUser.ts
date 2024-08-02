d996909e616a6d2c885971272eb8f3c0d72579b7
BeguAlexandruU <alexandru.begu@student.tuiasi.ro>
2024-08-02 05:05:19 +0300

    none

--- front/middleware/authUser.ts
+++ front/middleware/authUser.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 	const userStore = useUserStore()
 
     if (!userStore.accessToken) {
-        return navigateTo('/auth/login')
+        return await navigateTo('/auth/login')
     }
 
 })
