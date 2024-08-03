1802ed9dc8eb43ff08c7b2a537714579395285e9
BeguAlexandruU <alexandru.begu@student.tuiasi.ro>
2024-08-03 18:39:02 +0300

    show and copy referal link

--- front/pages/profile/referals.vue
+++ front/pages/profile/referals.vue
@@ -1,14 +1,32 @@
 <template>
 	<DashboardNavbar title="Referals" v-model="isSidebarOpen" />
 
-	<div>page content</div>
+	<div class="flex border-separate">
+		<div>
+			<p>My referal code:</p>
+			<p>Parent referal code:</p>
+		</div>
+		<div>
+			<p>{{ userStore.user.ref_code }}</p>
+			<p>{{ userStore.user.ref_code_parent }}</p>
+		</div>
+	</div>
+	<UButton @click="copyLink"> Copy invite link </UButton>
 </template>
 
 <script setup lang="ts">
 const { isSidebarOpen } = useDashboard()
+const userStore = useUserStore()
+
 definePageMeta({
 	title: 'Referals',
 	layout: 'profile',
 	middleware: ['auth-user'],
 })
+
+function copyLink() {
+	navigator.clipboard.writeText(
+		'http://localhost:3000/auth/register?ref=' + userStore.user.ref_code
+	)
+}
 </script>
