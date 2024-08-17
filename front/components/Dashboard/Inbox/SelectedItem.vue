6c5c4f09201fff95d6fc16d4cda3c8fb11b7035e
BeguAlexandruU <alexandru.begu@student.tuiasi.ro>
2024-08-17 23:48:24 +0300

    inbox page ui

---
+++ front/components/Dashboard/Inbox/SelectedItem.vue
@@ -0,0 +1,26 @@
+<template>
+	<div
+		v-if="selectedItemId != null"
+		class="hidden md:flex flex-col flex-1 gap-2 p-4"
+	>
+		<slot />
+	</div>
+
+	<div
+		v-else
+		class="hidden md:flex flex-1 justify-center items-center gap-2 p-4"
+	>
+		<UIcon name="i-heroicons-inbox" class="h-32 w-32 text-gray-600" />
+	</div>
+
+	<USlideover v-model="isSideoverItemOpen" class="flex md:hidden">
+		<div class="flex flex-col flex-1 gap-2 p-4">
+			<slot />
+		</div>
+	</USlideover>
+</template>
+
+<script setup>
+const selectedItemId = defineModel('selectedItemId')
+const isSideoverItemOpen = defineModel('isSideoverItemOpen')
+</script>
--- front/pages/profile/inbox.vue
+++ front/pages/profile/inbox.vue
@@ -1,13 +1,68 @@
 <template>
 	<DashboardNavbar title="Inbox" v-model="isSidebarOpen" />
 
-	<div>
-		<h1>Inbox</h1>
-		<ul>
-			<li v-for="obj in notificationStore.notifications">
-				{{ obj.title }}
-			</li>
-		</ul>
+	<!-- page content container  -->
+	<div class="flex flex-1 flex-row overflow-y-auto">
+		<!-- notifications list container -->
+		<div class="grid flex-1 border-r border-gray-800 overflow-y-auto">
+			<div
+				v-for="item in notificationStore.notifications"
+				class="flexflex-col-reverse"
+				@click="
+					() => {
+						selectedNotificationId =
+							notificationStore.notifications.indexOf(item)
+
+						isSideoverNotificationOpen = true
+					}
+				"
+			>
+				<UDivider />
+				<div
+					v-if="
+						notificationStore.notifications.indexOf(item) ==
+						selectedNotificationId
+					"
+					class="flex flex-col flex-1 gap-1 border-l-2 border-gray-700 bg-gray-800 px-3 py-4"
+				>
+					<span class="text-xl font-semibold">{{ item.title }}</span>
+					<span class="text-gray-400">{{ item.message }}</span>
+				</div>
+				<div
+					v-else
+					class="flex flex-col flex-1 gap-1 border-l-2 border-gray-900 hover:border-gray-600 bg-gray-900 hover:bg-gray-700 px-3 py-4"
+				>
+					<span class="text-xl font-semibold">{{ item.title }}</span>
+					<span class="text-gray-400">{{ item.message }}</span>
+				</div>
+			</div>
+		</div>
+
+		<!-- selected notification container -->
+		<DashboardInboxSelectedItem
+			v-model:selectedItemId="selectedNotificationId"
+			v-model:isSideoverItemOpen="isSideoverNotificationOpen"
+		>
+			<div class="flex gap-1.5">
+				<UButton
+					icon="material-symbols:close"
+					size="sm"
+					square
+					color="gray"
+					variant="ghost"
+					class="block md:hidden m-r-3"
+					@click="isSideoverNotificationOpen = false"
+				/>
+				<span class="text-3xl font-semibold">{{
+					notificationStore.notifications[selectedNotificationId]
+						?.title
+				}}</span>
+			</div>
+			<UDivider />
+			<span class="text-gray-400">{{
+				notificationStore.notifications[selectedNotificationId]?.message
+			}}</span>
+		</DashboardInboxSelectedItem>
 	</div>
 </template>
 
@@ -20,4 +75,7 @@ definePageMeta({
 	layout: 'profile',
 	middleware: ['auth-user'],
 })
+
+const isSideoverNotificationOpen = ref(false)
+const selectedNotificationId = ref()
 </script>
